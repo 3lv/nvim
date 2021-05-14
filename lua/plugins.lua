e7fb0195ea17a60bd7a3fd06d21b5e042bfaeaf7
3lv <cristian.vlad.vladutu@gmail.com>
2021-05-14 23:43:09 +0300

    GOOD

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -13,7 +13,7 @@ return require('packer').startup{ function()
 	use { '3lv/femboyscheme', config = 'vim.cmd[[colorscheme femboyscheme]]' }
 	use { '3lv/femboystatus', config = function() require('femboystatus').setup{ } end }
 	use { '3lv/femboyalt',    config = function() require('femboyalt') end }
-	--use { '3lv/femboycpp', ft = {'cpp'}, config = function() require('femboycpp').setup { } end }
+	use { '3lv/femboycpp', ft = {'cpp'}, config = function() require('femboycpp').setup { } end }
 	use { '3lv/femboyf',      config = function() require('femboyf').setup{ style = 'line'} end }
 	use { 'kyazdani42/nvim-tree.lua', config = function() require('config.nvim-tree') end }
 	use { 'nvim-telescope/telescope.nvim', cmd = { 'F', 'R', 'A' }, requires = { { 'nvim-lua/popup.nvim' }, { 'nvim-lua/plenary.nvim' } }, config = function() require('config.telescope') end }
