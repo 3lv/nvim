86b44c301d3f95ed97b96f5fc39f33f4797522fb
3lv <cristian.vlad.vladutu@gmail.com>
2021-05-14 23:40:04 +0300

    removed unecessary

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -9,17 +9,16 @@ end
 
 return require('packer').startup{ function()
 	use { 'wbthomason/packer.nvim' }
+	use { '3lv/femboyrainbow', config = function() require('femboyrainbow').start() end }
 	use { '3lv/femboyscheme', config = 'vim.cmd[[colorscheme femboyscheme]]' }
 	use { '3lv/femboystatus', config = function() require('femboystatus').setup{ } end }
 	use { '3lv/femboyalt',    config = function() require('femboyalt') end }
-	use { '3lv/femboycpp', ft = {'cpp'}, config = function() require('femboycpp').setup { } end }
+	--use { '3lv/femboycpp', ft = {'cpp'}, config = function() require('femboycpp').setup { } end }
 	use { '3lv/femboyf',      config = function() require('femboyf').setup{ style = 'line'} end }
 	use { 'kyazdani42/nvim-tree.lua', config = function() require('config.nvim-tree') end }
-	--use { 'nvim-telescope/telescope.nvim', cmd = 'F', requires = { { 'nvim-lua/popup.nvim', cmd = 'F' }, { 'nvim-lua/plenary.nvim', cmd = 'F' }}, config = function() require('config.telescope') end }
 	use { 'nvim-telescope/telescope.nvim', cmd = { 'F', 'R', 'A' }, requires = { { 'nvim-lua/popup.nvim' }, { 'nvim-lua/plenary.nvim' } }, config = function() require('config.telescope') end }
 	use { 'nvim-treesitter/nvim-treesitter', run = ':TSInstall cpp lua', config = function() require('config.nvim-treesitter') end }
 	use { 'neovim/nvim-lspconfig', ft = {'cpp'}, config = function() require('config.nvim-lspconfig') end }
-	use { 'glepnir/lspsaga.nvim', ft = {'cpp'}, config = function() require('config.lspsaga') end }
 	use { 'norcalli/nvim-colorizer.lua', cmd = 'ColorizerToggle', config = function() require('config.colorizer') end }
 	use { 'kyazdani42/nvim-web-devicons', config = function() require('config.nvim-web-devicons') end }
 end }
